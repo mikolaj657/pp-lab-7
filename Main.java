607152247a4d05afd4bbddb625374596d9a0ebdb
Mikołaj Kubicki <mikikubica@o2.pl>
2024-06-01 10:26:02 +0200

    zad4

--- Main.java
+++ Main.java
@@ -10,6 +10,9 @@ import javafx.scene.layout.VBox;
 import javafx.geometry.Insets;
 
 import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main extends Application {
     private TextField directoryPathField;
@@ -33,6 +36,7 @@ public class Main extends Application {
         browseButton.setOnAction(e -> browseDirectory());
 
         Button searchButton = new Button("Search");
+        searchButton.setOnAction(e -> searchFiles());
 
         HBox hBox = new HBox(10, directoryPathField, browseButton);
         hBox.setPadding(new Insets(10));
@@ -53,6 +57,55 @@ public class Main extends Application {
         if (selectedDirectory != null) {
             directoryPathField.setText(selectedDirectory.getAbsolutePath());
         }
+    }
+
+    private void searchFiles() {
+        String directoryPath = directoryPathField.getText();
+        String searchPhrase = searchField.getText();
+
+        if (directoryPath.isEmpty()) {
+            resultArea.setText("Please provide a directory path.");
+            return;
+        }
+
+        File directory = new File(directoryPath);
+        if (!directory.isDirectory()) {
+            resultArea.setText("The provided path is not a directory.");
+            return;
+        }
+
+        StringBuilder results = new StringBuilder();
+        searchInDirectory(directory, searchPhrase, results);
+        resultArea.setText(results.toString());
+    }
+
+    private void searchInDirectory(File directory, String searchPhrase, StringBuilder results) {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    if (containsPhrase(file, searchPhrase)) {
+                        results.append(file.getAbsolutePath()).append("\n");
+                    }
+                } else if (file.isDirectory()) {
+                    searchInDirectory(file, searchPhrase, results);
+                }
+            }
+        }
+    }
+
+    private boolean containsPhrase(File file, String searchPhrase) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.contains(searchPhrase)) {
+                    return true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     public static void main(String[] args) {
